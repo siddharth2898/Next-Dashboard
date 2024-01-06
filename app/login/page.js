b78f61c30ecaabdaa110dda98ca644bdd58407f6
siddharth <sid28071998@gmail.com>
2024-01-06 15:50:54 +0530

    version 2

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
+import Logo from '../ui/logo';
+import LoginForm from '../ui/login-form';
 
 export default function LoginPage() {
     return (
@@ -7,7 +7,7 @@ export default function LoginPage() {
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
                     <div className="w-32 text-white md:w-36">
-                        <AcmeLogo />
+                        <Logo />
                     </div>
                 </div>
                 <LoginForm />
